b8c239f86800252ef41f5b0984f4c780685207fb
Abhirup Banerjee <43820709+abhirupbanerjee97@users.noreply.github.com>
2020-02-23 00:29:27 +0530

    WishList Partially Done

--- src/components/wishList/WishList.js
+++ src/components/wishList/WishList.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react'
 import {AuthContext} from "../../context/AuthContext";
 import {Redirect} from "react-router";
+import Style from './WishList.module.css';
 
 const WishList = (props) => {
     const [isLoggedIn, setIsLoggedIn, user, setUser] = useContext(AuthContext)
@@ -10,7 +11,17 @@ const WishList = (props) => {
         )
     }
     else
-        return <h2>Wish List</h2>
+        return (
+            <div className="flex-container">
+                <div>
+                    <img src="Fossil Sport.jpg" alt="Fossil Sport Image"/>
+                </div>
+                <div>
+                    <h2>Fossil Sport for Men 43mm Black Color</h2>
+                    <h2>Rs. 11995 <strike><h3>Rs. 18000</h3></strike></h2>
+                </div>
+            </div>
+        )
 }
 
 export default WishList
